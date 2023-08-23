fdfeac768572d5bd94c10797e53c1f8ec4516898
Siyuan <suquark@gmail.com>
2023-08-22 17:16:23 -0700

    sync with upstream

--- sky/provision/aws/instance.py
+++ sky/provision/aws/instance.py
@@ -5,7 +5,11 @@ import copy
 import time
 
 import botocore
+from botocore import config
 from botocore import exceptions as boto_exceptions
+
+from sky import status_lib
+from sky.adaptors import aws
 
 # We still have to depend on Ray logger, because currently
 # our logger does not support the same coloring schema as Ray.
@@ -23,6 +27,8 @@ BOTO_CREATE_MAX_RETRIES = 5
 TAG_RAY_CLUSTER_NAME = 'ray-cluster-name'
 TAG_RAY_NODE_TYPE = 'ray-node-type'  # legacy tag for backward compatibility
 TAG_SKYPILOT_HEAD_NODE = 'skypilot-head-node'
+BOTO_MAX_RETRIES = 12
+
 
 logger = sky_logging.init_logger(__name__)
 
@@ -38,6 +44,20 @@ logger = sky_logging.init_logger(__name__)
 # Data transfer within the same region but different availability zone
 #  costs $0.01/GB:
 # https://aws.amazon.com/ec2/pricing/on-demand/#Data_Transfer_within_the_same_AWS_Region
+
+
+def _default_ec2_resource(region: str) -> Any:
+    return aws.resource(
+        'ec2',
+        region_name=region,
+        config=config.Config(retries={'max_attempts': BOTO_MAX_RETRIES}))
+
+
+def _cluster_name_filter(cluster_name: str) -> List[Dict[str, Any]]:
+    return [{
+        'Name': f'tag:{TAG_RAY_CLUSTER_NAME}',
+        'Values': [cluster_name],
+    }]
 
 
 def describe_instances(region: str) -> Dict:
@@ -357,50 +377,109 @@ def _filter_instances(ec2, filters: List[Dict[str, Any]],
     return instances
 
 
-def stop_instances(region: str,
-                   cluster_name: str,
-                   included_instances: Optional[List[str]] = None,
-                   excluded_instances: Optional[List[str]] = None) -> None:
+# TODO(suquark): Does it make sense to not expose this and always assume
+# non_terminated_only=True?
+# Will there be callers who would want this to be False?
+# stop() and terminate() for example already implicitly assume non-terminated.
+def query_instances(
+    cluster_name: str,
+    provider_config: Optional[Dict[str, Any]] = None,
+    non_terminated_only: bool = True,
+) -> Dict[str, Optional[status_lib.ClusterStatus]]:
     """See sky/provision/__init__.py"""
-    ec2 = utils.create_resource('ec2', region=region)
-    filters = [
+    assert provider_config is not None, (cluster_name, provider_config)
+    region = provider_config['region']
+    ec2 = _default_ec2_resource(region)
+    filters = _cluster_name_filter(cluster_name)
+    instances = ec2.instances.filter(Filters=filters)
+    status_map = {
+        'pending': status_lib.ClusterStatus.INIT,
+        'running': status_lib.ClusterStatus.UP,
+        # TODO(zhwu): stopping and shutting-down could occasionally fail
+        # due to internal errors of AWS. We should cover that case.
+        'stopping': status_lib.ClusterStatus.STOPPED,
+        'stopped': status_lib.ClusterStatus.STOPPED,
+        'shutting-down': None,
+        'terminated': None,
+    }
+    statuses = {}
+    for inst in instances:
+        status = status_map[inst.state['Name']]
+        if non_terminated_only and status is None:
+            continue
+        statuses[inst.id] = status
+    return statuses
+
+
+def stop_instances(
+    cluster_name: str,
+    provider_config: Optional[Dict[str, Any]] = None,
+    worker_only: bool = False,
+) -> None:
+    """See sky/provision/__init__.py"""
+    assert provider_config is not None, (cluster_name, provider_config)
+    region = provider_config['region']
+    ec2 = _default_ec2_resource(region)
+    filters: List[Dict[str, Any]] = [
         {
             'Name': 'instance-state-name',
             'Values': ['pending', 'running'],
         },
-        {
-            'Name': f'tag:{TAG_RAY_CLUSTER_NAME}',
-            'Values': [cluster_name],
-        },
+        *_cluster_name_filter(cluster_name),
     ]
-    # TODO(suquark): wait pending clusters?
-    instances = _filter_instances(ec2, filters, included_instances,
-                                  excluded_instances)
+    if worker_only:
+        filters.append({
+            'Name': f'tag:{TAG_RAY_NODE_TYPE}',
+            'Values': ['worker'],
+        })
+    instances = _filter_instances(ec2, filters, None, None)
     instances.stop()
+    # TODO(suquark): Currently, the implementation of GCP and Azure will
+    #  wait util the cluster is fully terminated, while other clouds just
+    #  trigger the termination process (via http call) and then return.
+    #  It's not clear that which behavior should be expected. We will not
+    #  wait for the termination for now, since this is the default behavior
+    #  of most cloud implementations (including AWS).
 
 
-def terminate_instances(region: str,
-                        cluster_name: str,
-                        included_instances: Optional[List[str]] = None,
-                        excluded_instances: Optional[List[str]] = None) -> None:
+def terminate_instances(
+    cluster_name: str,
+    provider_config: Optional[Dict[str, Any]] = None,
+    worker_only: bool = False,
+) -> None:
     """See sky/provision/__init__.py"""
-    ec2 = utils.create_resource('ec2', region=region)
+    assert provider_config is not None, (cluster_name, provider_config)
+    region = provider_config['region']
+    ec2 = _default_ec2_resource(region)
     filters = [
         {
             'Name': 'instance-state-name',
             # exclude 'shutting-down' or 'terminated' states
             'Values': ['pending', 'running', 'stopping', 'stopped'],
         },
-        {
-            'Name': f'tag:{TAG_RAY_CLUSTER_NAME}',
-            'Values': [cluster_name],
-        },
+        *_cluster_name_filter(cluster_name),
     ]
-    # TODO(suquark): wait pending clusters?
+    if worker_only:
+        filters.append({
+            'Name': f'tag:{TAG_RAY_NODE_TYPE}',
+            'Values': ['worker'],
+        })
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/ec2.html#EC2.Instance
-    instances = _filter_instances(ec2, filters, included_instances,
-                                  excluded_instances)
+    instances = _filter_instances(ec2, filters, None, None)
     instances.terminate()
+    if 'ports' not in provider_config:
+        return
+    # If ports are specified, we need to delete the newly created Security
+    # Group. Here we wait for all instances to be terminated, since the
+    # Security Group dependent on them.
+    for instance in instances:
+        instance.wait_until_terminated()
+    # TODO(suquark): Currently, the implementation of GCP and Azure will
+    #  wait util the cluster is fully terminated, while other clouds just
+    #  trigger the termination process (via http call) and then return.
+    #  It's not clear that which behavior should be expected. We will not
+    #  wait for the termination for now, since this is the default behavior
+    #  of most cloud implementations (including AWS).
 
 
 def wait_instances(region: str, cluster_name: str, state: str) -> None:
